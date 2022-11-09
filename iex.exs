055bb1ede7fe17ccb487d5bddbe9ba9613d79d35
Kaarel Tinn <kaareltinn@hotmail.com>
2022-11-09 11:22:24 +0200

    Add Helpers module to iex.exs

--- iex.exs
+++ iex.exs
@@ -1,15 +1,39 @@
 IEx.configure(
   colors: [
     enabled: true,
-    eval_result: [ :cyan, :bright ],
-    eval_error:  [ :light_magenta ],
+    eval_result: [:cyan, :bright],
+    eval_error: [:light_magenta]
   ],
-  default_prompt: [
-    "\r\e[38;5;220m",         # a pale gold
-    "%prefix",                # IEx context
-    "\e[38;5;112m(%counter)", # forest green expression count
-    "\e[38;5;220m>",          # gold ">"
-    "\e[0m",                  # and reset to default color
-  ]
-  |> IO.chardata_to_string    # (1)
+  default_prompt:
+    [
+      # a pale gold
+      "\r\e[38;5;220m",
+      # IEx context
+      "%prefix",
+      # forest green expression count
+      "\e[38;5;112m(%counter)",
+      # gold ">"
+      "\e[38;5;220m>",
+      # and reset to default color
+      "\e[0m"
+    ]
+    # (1)
+    |> IO.chardata_to_string()
 )
+
+defmodule Helpers do
+  def copy(term) do
+    text =
+      if is_binary(term) do
+        term
+      else
+        inspect(term, limit: :infinity, pretty: true)
+      end
+
+    port = Port.open({:spawn, "pbcopy"}, [])
+    true = Port.command(port, text)
+    true = Port.close(port)
+
+    :ok
+  end
+end

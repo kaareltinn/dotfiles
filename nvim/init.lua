49fabb6a4b8df0ac9571e41cd800ca2714bfcc25
Kaarel Tinn <kaarel.tinn@every-pay.com>
2024-11-04 17:14:47 +0200

    feat: Update init.lua
    * Add aerial
    * Add yaml plugin
    * Add Avante
    * Add CopilotChat
    * Other improvements

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,7 +40,7 @@ vim.opt.softtabstop = 0
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 
-vim.opt.laststatus = 2
+vim.opt.laststatus = 3
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -192,6 +192,25 @@ require("lazy").setup({
     end
   },
   {'nvim-treesitter/playground'},
+  {
+    'kchmck/vim-coffee-script'
+  },
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    dependencies = {
+       "nvim-treesitter/nvim-treesitter",
+       "nvim-tree/nvim-web-devicons"
+    },
+  },
+  {
+    "cuducos/yaml.nvim",
+    ft = { "yaml" }, -- optional
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+  },
   {'arcticicestudio/nord-vim'},
   {'tpope/vim-fugitive'},
   {'tpope/vim-surround'},
@@ -208,16 +227,72 @@ require("lazy").setup({
   {'L3MON4D3/LuaSnip'},
   {'honza/vim-snippets'},
   {'mattn/emmet-vim'},
+  {'github/copilot.vim'},
   {
-    'Exafunction/codeium.vim',
-    config = function ()
-      vim.g.codeium_manual = true
-      map('i', '<C-i>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-      map('i', '<C-j>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-      map('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-      map('i', '<C-c>', function() return vim.fn['codeium#Complete']() end, { expr = true, silent = true })
-    end
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+      provider = 'copilot',
+      mappings = {
+        ask = '<leader>ua',
+        edit = '<leader>ue',
+        refresh = '<leader>ur',
+        jump = {
+          next = '<M-j>',
+          prev = '<M-k>',
+        },
+      }
+
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "github/copilot.vim", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
   },
+  -- {
+  --   'Exafunction/codeium.vim',
+  --   config = function ()
+  --     vim.g.codeium_manual = true
+  --     map('i', '<C-i>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+  --     map('i', '<C-j>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+  --     map('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+  --     map('i', '<C-c>', function() return vim.fn['codeium#Complete']() end, { expr = true, silent = true })
+  --   end
+  -- },
   {'mhinz/vim-mix-format'},
   {
     'nvim-lualine/lualine.nvim',
@@ -272,16 +347,33 @@ end)
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {
-    'tsserver',
+    -- 'tsserver',
     'solargraph',
     'elixirls',
-    'gopls',
+    -- 'gopls',
   },
   handlers = {
     lsp_zero.default_setup,
   },
 })
+local lspconfig = require("lspconfig")
 
+lspconfig.solargraph.setup({
+  settings = {
+    solargraph = {
+      diagnostics = true
+    }
+  }
+})
+
+-- Aerial
+require('aerial').setup({
+  backends = { 'treesitter', 'lsp' }
+})
+require('telescope').load_extension('aerial')
+
+map('n', '<leader>ae', '<cmd>AerialToggle!<CR>')
+map('n', '<leader>fa', '<cmd>:Telescope aerial<CR>')
 
 -- GO
 local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
@@ -346,23 +438,35 @@ luasnip.filetype_extend("typescriptreact", {"javascript"})
 map('i', '<C-l>', luasnip.expand)
 
 -- Telescope
+require('telescope').setup({
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  },
+  extensions = {
+    aerial = {
+      show_columns = 'both',
+    },
+  },
+})
+
 local builtin = require('telescope.builtin')
 map('n', '<leader>fs', builtin.find_files)
 map('n', '<leader>fg', builtin.live_grep)
 map('n', '<leader>fb', builtin.buffers)
 map('n', 'K', ":execute 'Telescope live_grep default_text=' . expand('<cword>')<cr>")
 
-require('telescope').load_extension 'pomodori'
-
-map('n', '<leader>pt', function()
-  require('telescope').extensions.pomodori.timers()
-end, { desc = "Manage Pomodori timers" })
-
-
 -- Lualine
 require('lualine').setup({
   options = {
     theme = 'nord',
+  },
+  sections = {
+    lualine_c = { {'filename', path=1} }
+  },
+  inactive_sections = {
+    lualine_c = { {'filename', path=1} }
   }
 })
 
@@ -388,6 +492,11 @@ map('t', '<C-o>', '<C-\\><C-n>', { noremap = false })
 vim.g['test#strategy'] = 'neovim'
 vim.g['test#neovim#term_position'] = '20'
 vim.g['test#ruby#rspec#executable'] = 'bundle exec rspec'
+
+-- yaml.nvim
+map('n', '<leader>yav', ':YAMLView<CR>')
+map('n', '<leader>yat', ':YAMLTelescope<CR>')
+map('n', '<leader>yaq', ':YAMLQuickfix<CR>')
 
 -- other.vim
 require("other-nvim").setup({

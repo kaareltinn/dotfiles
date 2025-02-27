1bd10775cad9d29ff585d93ea9d677ecb48133f9
Kaarel Tinn <kaareltinn@hotmail.com>
2025-02-27 09:22:54 +0200

    feat(lsp)!: migrate from VLS to Volar for Vue development
    BREAKING CHANGE: Switches Vue language server from VLS to Volar and configures TypeScript LSP to work with Vue files using @vue/typescript-plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,7 +22,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {
     'ts_ls',
-    'vls',
+    'volar',
     -- 'solargraph',
     'elixirls',
     'terraformls',
@@ -32,6 +32,37 @@ require('mason-lspconfig').setup({
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
+    end,
+    volar = function()
+      require('lspconfig').volar.setup({})
+    end,
+    ts_ls = function()
+      local vue_typescript_plugin = require('mason-registry')
+        .get_package('vue-language-server')
+        :get_install_path()
+        .. '/node_modules/@vue/language-server'
+        .. '/node_modules/@vue/typescript-plugin'
+
+      require('lspconfig').ts_ls.setup({
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vue_typescript_plugin,
+              languages = {'javascript', 'typescript', 'vue'}
+            },
+          }
+        },
+        filetypes = {
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
+          'vue',
+        },
+      })
     end,
   }
 })
